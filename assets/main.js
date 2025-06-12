1fdb6a811cfd4242c886b254eaef825dde1585d4
Pablo Fretes <pablofretes@gmail.com>
2025-06-12 14:05:49 -0300

    fix(js): mal cargado

--- assets/main.js
+++ assets/main.js
@@ -1,26 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {    
     const nav = document.querySelector('nav');
-    const items = document.querySelectorAll('.experiencia-item');
+   
+    // const items = document.querySelectorAll('.experiencia-item');
 
     function updateActiveSection() {
         
-        const windowMiddle = window.innerHeight / 2;
+        // const windowMiddle = window.innerHeight / 2;
         
-        items.forEach(item => {
-            const rect = item.getBoundingClientRect();
-            const itemMiddle = rect.top + (rect.height / 2);
+        // items.forEach(item => {
+        //     const rect = item.getBoundingClientRect();
+        //     const itemMiddle = rect.top + (rect.height / 2);
             
-            if (Math.abs(itemMiddle - windowMiddle) < rect.height / 2) {
-                item.classList.add('active');
-            } else {
-                item.classList.remove('active');
-            }
-        });
+        //     if (Math.abs(itemMiddle - windowMiddle) < rect.height / 2) {
+        //         item.classList.add('active');
+        //     } else {
+        //         item.classList.remove('active');
+        //     }
+        // });
+
+        const scrollThreshold = 0.7 * window.innerHeight;
         // Actualizar nav background
-        if (window.scrollY > 0) {
-            nav.classList.add('scrolled');
-        } else {
+        if (window.scrollY > 90 && window.scrollY < scrollThreshold) {
             nav.classList.remove('scrolled');
+        } else {
+            nav.classList.add('scrolled');
         }
     }
 
